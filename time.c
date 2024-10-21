6a9f7c1bdc1647591b510d79d2f26d5c954ed584
Aniaa-Johnson <aajohnson20@students.desu.edu>
2024-10-20 23:17:57 -0400

    ok

--- time.c
+++ time.c
@@ -1,8 +1,9 @@
 #include <sys/time.h>
+#include "lab2.h"
 
-double elapsed_time(struct timeval* start_time, struct timeval* end_time){
-    //TODO: return the difference between end_time and start_time.
-    double startTimeMicro = start_time->tv_sec*1000000 + start_time->tv_usec; 
-    double endTimeMicro =end_time->tv_sec*1000000 + end_time->tv_usec;
-    return (endTimeMicro-startTimeMicro)/1000000.00;
+// Function to calculate the elapsed time
+double elapsed_time(struct timeval* start, struct timeval* end) {
+    double start_sec = start->tv_sec + (start->tv_usec / 1000000.0);
+    double end_sec = end->tv_sec + (end->tv_usec / 1000000.0);
+    return end_sec - start_sec;
 }
